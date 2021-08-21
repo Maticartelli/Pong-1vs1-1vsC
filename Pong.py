7b10e8c6ef3432c53278410cd7d5f72959a0e333
Matias <mtias98bien@gmail.com>
2021-08-20 22:03:41 -0300

    Primera versión terminada, me gustaría hacer un lobby y evitar que los jugadores colisionen con el techo y el piso

--- Pong.py
+++ Pong.py
@@ -7,16 +7,116 @@ window.title("Pong, The game")
 #Ponemos el color
 window.bgcolor("black")
 #Las dimensiones
-window.setup(width=8000 , height=600)
+window.setup(width=800 , height=600)
 window.tracer(0)
 
 #Jugador uno
 J1 = turtle.Turtle()
 J1.speed(0)
 J1.shape("square")
-J1.color("white")
-#J1.penup()
+J1.color("violet")
+J1.penup()
 J1.goto(-350,0)
+J1.shapesize(stretch_wid=8,stretch_len=1)
+
+#Jugador dos
+J2 = turtle.Turtle()
+J2.speed(0)
+J2.shape("square")
+J2.color("violet")
+J2.penup()
+J2.goto(350,0)
+J2.shapesize(stretch_wid=8,stretch_len=1)
+
+
+#Pelota
+pelota = turtle.Turtle()
+pelota.speed(0)
+pelota.shape("circle")
+pelota.color("white")
+pelota.penup()
+pelota.goto(0,0)
+pelota.dx = 0.3
+pelota.dy = 0.3
+
+#División
+division = turtle.Turtle()
+division.color("red")
+division.goto(0,400)
+division.goto(0,-400)
+
+#Marcador
+
+Marc = turtle.Turtle()
+Marc.speed(0)
+Marc.color("blue")
+Marc.penup()
+Marc.hideturtle()
+Marc.goto(0,260)
+Marc.write("Jugador 1: 0        Jugador 2: 0", align="center", font=("Courier",24,"normal"))
+
+#
+Marc1 = 0
+Marc2 = 0
+#Movimientos
+def J1_up():
+    y = J1.ycor()
+    y +=20
+    J1.sety(y)
+def J1_down():
+    y = J1.ycor()
+    y -=20
+    J1.sety(y)
+    
+def J2_up():
+    y = J2.ycor()
+    y +=20
+    J2.sety(y)
+def J2_down():
+    y = J2.ycor()
+    y -=20
+    J2.sety(y)
+    
+#Teclado
+window.listen()
+window.onkeypress(J1_up, "w")
+window.onkeypress(J1_down, "s")
+window.onkeypress(J2_up, "Up")
+window.onkeypress(J2_down, "Down")
+
 
 while True:
     window.update()
+    
+    pelota.setx(pelota.xcor() + pelota.dx)
+    pelota.sety(pelota.ycor() + pelota.dy)
+    
+    #Bordes
+    if pelota.ycor() > 290:
+        pelota.dy *= -1
+        
+    if pelota.ycor() < -290:
+        pelota.dy *= -1
+    
+    if pelota.xcor() > 390:
+        pelota.goto(0,0)
+        pelota.dx *= -1
+        Marc1 +=1
+        Marc.clear()
+        Marc.write("Jugador 1: {}        Jugador 2: {}".format(Marc1,Marc2), align="center", font=("Courier",24,"normal"))
+    if pelota.xcor() < -390:
+        pelota.goto(0,0)
+        pelota.dx *= -1
+        Marc2 += 1
+        Marc.clear()
+        Marc.write("Jugador 1: {}        Jugador 2: {}".format(Marc1,Marc2), align="center", font=("Courier",24,"normal"))
+    if ((pelota.xcor() > 340 and pelota.xcor() < 350)
+            and (pelota.ycor() < J2.ycor() + 80 
+                 and pelota.ycor() > J2.ycor()-80)):
+        pelota.dx *=-1
+    
+    if ((pelota.xcor() < -340 and pelota.xcor() > -350)
+            and (pelota.ycor() < J1.ycor() + 80 
+                 and pelota.ycor() > J1.ycor()-80)):
+        pelota.dx *=-1
+    
