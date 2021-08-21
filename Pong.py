cf6be05e61c603068237ab6b2b885eec9628bb69
Matias <mtias98bien@gmail.com>
2021-08-20 21:23:40 -0300

    Movimientos de los jugadores

--- Pong.py
+++ Pong.py
@@ -1,5 +1,22 @@
 import turtle
+
+#Creamos la ventana para el juego
 window = turtle.Screen()
+#Ponemos el título
 window.title("Pong, The game")
+#Ponemos el color
 window.bgcolor("black")
-window.
+#Las dimensiones
+window.setup(width=8000 , height=600)
+window.tracer(0)
+
+#Jugador uno
+J1 = turtle.Turtle()
+J1.speed(0)
+J1.shape("square")
+J1.color("white")
+#J1.penup()
+J1.goto(-350,0)
+
+while True:
+    window.update()
